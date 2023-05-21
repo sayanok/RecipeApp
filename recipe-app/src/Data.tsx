2874f51f391b685e407c96c057bc954b3aad3e2c
sayanok <rng.jhn.osca@gmail.com>
2023-05-21 12:36:51 +0900

    add すべての機能を追加

---
+++ recipe-app/src/Data.tsx
@@ -0,0 +1,22 @@
+const RecipeData = [
+  {
+    id: 1,
+    title: "ハンバーグ",
+    mealType: "晩ごはん",
+    numberOfPeople: 3,
+    difficultyLevel: "advanced",
+    listOfIngredients: ["ひき肉", "玉ねぎ", "パン粉"],
+    preparationSteps: ["材料を切る", "混ぜる", "成形する", "焼く"],
+  },
+  {
+    id: 2,
+    title: "サラダ",
+    mealType: "ランチ",
+    numberOfPeople: 3,
+    difficultyLevel: "beginner",
+    listOfIngredients: ["レタス", "きゅうり", "お好みのドレッシング", "プチトマト"],
+    preparationSteps: ["レタスを洗う", "レタスをちぎる", "きゅうりを切る", "盛り付ける", "ドレッシングをかける"],
+  },
+];
+
+export default RecipeData;
